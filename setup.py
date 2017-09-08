018a6e57b042050490d0219d2f19b150ace1e99c
Jonathan Johannemann <JonathanJohannemann@gmail.com>
2017-09-08 17:32:34 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='libgeohash',
+    packages=['libgeohash'],
+    version='0.1',
+    description='Transform GeoJSON shape to a list of geohashes',
+    author='Jerry Xu and Jonathan Johannemann',
+    author_email='info@sstm2.com',
+    url='https://github.com/sstm2/geohashlib',
+    download_url='https://github.com/sstm2/geohashlib',
+    keywords=['geohash', 'geo'],
+    classifiers=[],
+)
